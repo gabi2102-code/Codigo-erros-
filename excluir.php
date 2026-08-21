452024d0295e260db0ca4eb40def1df39241cf15
gabi2102-code <gabriela_b_ferreira@estudante.sesisenai.org.br>
2026-08-21 08:54:59 -0300

    add- adicionei o index

---
+++ excluir.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_GET['excluir'])) {
+
+    $id = $_GET['excluir'];
+
+    $sql = "DELETE FROM usuarios WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+
+    header("Location: index.php");
+    exit;
+?>
