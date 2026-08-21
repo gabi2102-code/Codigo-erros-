5a3d2e4d7f8d1295640cdb4a13db054fa999d53e
gabi2102-code <gabriela_b_ferreira@estudante.sesisenai.org.br>
2026-08-21 07:53:35 -0300

    add-  Mudei alguns erros que encontrei

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "crud_aula";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
+};
+?>
